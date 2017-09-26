22318fa762ef3159f260c3cfd68b65cb28393902
gmelnik <Galina.Melnik@homecredit.ru>
2017-09-26 09:50:59 +0300

    Unable to identify the primary key for table 'department'.

---
+++ DepartmentWebApi/code/Model1/DepartmentDBContext1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace code.Model1
+{
+    public partial class DepartmentDBContext1 : DbContext
+    {
+        // Unable to generate entity type for table 'department'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite(@"Data Source=D:\\\\My\\\\dev\\\\SPA\\\\db\\\\db2.db;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
