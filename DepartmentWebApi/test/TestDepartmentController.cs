f2803a2c9401ef2f12ab8ccb5be9f9cb5094be77
gmelnik <Galina.Melnik@homecredit.ru>
2017-10-19 15:34:12 +0300

    TestGetById not passed

--- DepartmentWebApi/test/TestDepartmentController.cs
+++ DepartmentWebApi/test/TestDepartmentController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 using Moq;
 using DepartmentWebApi.code.Interfaces;
@@ -11,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DepartmentWebApi.Tests
 {
+    // todo : run all tests automatically when build|run main project
     public class TestDepartmentController
     {
         [Fact]
@@ -34,14 +36,31 @@ namespace DepartmentWebApi.Tests
         public async void TestGetById(long id, Type t)
         {
             var mockDepartmentRepository = new Mock<IDepartmentRepository>();
-            var mockLogger = new Mock<ILogger<DepartmentController>>();
-            mockDepartmentRepository.Setup(repo=> repo.GetAsync(It.IsAny(int))).Returns(Task.Run(()=>GetFakeDepartment()));
+            var mockLogger = new Mock<ILogger<DepartmentController>>();         
+// можно ли переписать без TasdkRun а с await GetFakeDepartment
+            mockDepartmentRepository.SetupSequence(repo=> repo.GetAsync(id)).Returns(Task.Run(()=>{
+                return Task.Run(() =>{
+                    return new Department{Id=0, Title="It"};});
+                }))
+                .Returns(Task.Run(()=>{
+                return Task.Run(() =>{
+                    return new Department{Id=1, Title="Marketing"};});
+                }))
+                .Returns(Task.Run(()=>{
+                return Task.Run(() =>{
+                    return new Department{Id=2, Title="Accountant Department"};});
+                }));
+                
             var dController = new DepartmentController(mockDepartmentRepository.Object, mockLogger.Object);
-            var result = await dController.Get();
+            var result = await dController.Get(id);
+            Assert.IsType(t, result.GetType());
 
-            var objectResult = Assert.IsType<ObjectResult>(result);
-            var departments = Assert.IsType<List<Department>>(objectResult.Value);
-            Assert.Equal(departments.Count, 3);
+            if (t == typeof(ObjectResult)){
+                var objectResult = Assert.IsType<ObjectResult>(result);
+                var department = Assert.IsType<Department>(objectResult.Value);
+                Assert.Equal(department.Id, 1);
+                Assert.Equal(department.Title, "Marketing");
+            }
         }
 
         private async Task<List<Department>> GetFakeDepartment()
